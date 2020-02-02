efdfc5d20992944df728ee6f1f2b17eb5005c87c
Nasser <nasser-eddin.nasser@stud.uni-bamberg.de>
2020-02-02 20:21:28 +0100

    adap-hw13

--- src/main/java/org/wahlzeit/model/Car/Car.java
+++ src/main/java/org/wahlzeit/model/Car/Car.java
@@ -4,18 +4,33 @@ import org.wahlzeit.services.DataObject;
 
 import java.util.UUID;
 
- /**
+/**
  * Instantiation of car that use to create a car photo.
  * Each car has a type that is changeable and an unique ID that is unchangeable.
  * A car can be directly created from its constructor by adding a type of it, the Car Manager will check if the car already exist or it will add new car to carList "HashTable".
  * By adding a type to car, the Car Manager will check if the type already exist or it will add new type to typesList "HashTable".
-
+ * <p>
  * 1- delegation of object creation: separate-object
  * 2- Selection of concrete class: by-mapping in CarManager
  * 3- Configuration of class mapping: in-code
  * 4- Instantiation of concrete class: in-code
  * 5- Initialization of new object: default
  * 6- Building of object structure: default
+ * <p>
+ * -------
+ * CW #14
+ * <p>
+ * Car with CarType:
+ * Collaboration (Type Object), it provides information to all instances of a car type.
+ * It binds the Car object(the base object) with the CarType (type)
+ * <p>
+ * with CarPhoto:
+ * Collaboration (client service), it provides main domain functionality.
+ * It binds the CarPhoto (client role) with the Car (service)
+ * <p>
+ * Car with CarManager:
+ * Collaboration (manager element), it centralize object management in CarManagement.
+ * It binds the Car (element role) with the CarManager (manager role).
  */
 
 public class Car extends DataObject {
@@ -28,7 +43,6 @@ public class Car extends DataObject {
         this.type = CarManager.getInstance().getCarType(carType);
         CarManager.getInstance().getCar(this);
         carID = UUID.randomUUID().toString();
-        ;
     }
 
     public CarType getType() {
